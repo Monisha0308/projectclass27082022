0f0309da2aece3dfcdbec7d2f0439cc755f648f0
Monisha0308 <monisha03082000@gmail.com>
2022-08-27 21:31:01 +0530

    initial commit

---
+++ src/test/java/com/base/employeedetails.java
@@ -0,0 +1,60 @@
+package com.base;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Vector;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public  class employeedetails  {
+	
+	private void employeename() {
+		System.out.println("hloo");
+
+	}
+	
+public static void main(String[] args) {
+		
+
+	}
+ 
+ 
+ 
+ 
+ 
+	}
+	
+	 
+ 
+	 
+	 
+	 
+
+	
+	 
+	 
+
+	
+		
+		
+	
+	
+
+
+
+
